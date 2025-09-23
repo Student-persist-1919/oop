75cd13087879ff0b3f6a7016b7544321eb01f84a
Nguyen-Quang-Minh-persist <minhnq1802@gmail.com>
2025-09-23 17:39:10 +0700

    First commit: Add HW1, HW2, HW3

---
+++ HW1/Introduction.java
@@ -0,0 +1,26 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.print("Nguyễn Quang Minh\t" + "24020235\t" + "INT2204_1\t" + "Student-persist-1919\t" + "24020235@vnu.edu.vn\n");
+        System.out.print(
+                        "9 bottles of beer on the wall, 9 bottles of beer.\n" +
+                        "Take one down, pass it around, \n" +
+                        "8 bottles of beer on the wall, 9 bottles of beer.\n" +
+                        "Take one down, pass it around, \n" +
+                        "7 bottles of beer on the wall, 9 bottles of beer.\n" +
+                        "Take one down, pass it around, \n" +
+                        "6 bottles of beer on the wall, 9 bottles of beer.\n" +
+                        "Take one down, pass it around, \n" +
+                        "5 bottles of beer on the wall, 9 bottles of beer.\n" +
+                        "Take one down, pass it around, \n" +
+                        "4 bottles of beer on the wall, 9 bottles of beer.\n" +
+                        "Take one down, pass it around, \n" +
+                        "3 bottles of beer on the wall, 9 bottles of beer.\n" +
+                        "Take one down, pass it around, \n" +
+                        "2 bottles of beer on the wall, 9 bottles of beer.\n" +
+                        "Take one down, pass it around, \n" +
+                        "1 bottles of beer on the wall, 9 bottles of beer.\n" +
+                        "Take one down, pass it around, \n" +
+                        "No more bottles of beer on the wall.");
+    }
+
+}
