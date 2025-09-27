a575ba60b86bbdfea53ac28aab0f5bbfead1e764
Nguyen-Quang-Minh-persist <minhnq1802@gmail.com>
2025-09-27 09:41:46 +0700

    Cập nhật HW2: xóa bản cũ, thêm bản mới từ kieuvantuyen01/oop

--- HW2/src/main/java/Appearances.java
+++ HW2/src/main/java/Appearances.java
@@ -4,7 +4,7 @@ public class Appearances {
 	
 	/**
 	 * Returns the number of elements that appear the same number
-	 * of times in both collections. Static method. (see handout).
+	 * of time in both collections. Static method. (see handout).
 	 * @return number of same-appearance elements
 	 */
 	public static <T> int sameCount(Collection<T> a, Collection<T> b) {
